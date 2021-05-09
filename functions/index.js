903613ba126fd340a2f22a349b85e0e46908fefb
Amanda Yu20 <amanda_yu2002@live.com>
2021-05-09 00:30:19 -0400

    finished end to end photo creation and payment process for seller and buyer

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,150 @@ const { app, firestore } = require('firebase-admin');
 
 admin.initializeApp();
 
+//This function gets called by stripe webhook on succesful payment
+exports.paymentIntentResultHandler = functions.https.onRequest(async (req, res) => {
+
+  //gets the stripe signature to make sure the request is coming from stripe
+  const sig = req.headers['stripe-signature'];
+  let event;
+
+  console.log('starting constructEvent...');
+
+  try {
+    //this is the variable that stores all relevant information for the payment intent
+    event = await stripe.webhooks.constructEvent(req.rawBody, sig, endpointSecret);
+
+  }
+  catch (err) {
+    res.status(400).send(err);
+    console.error(err.message);
+    return;
+  }
+
+  if (event.type === 'payment_intent.succeeded') {
+
+    try {
+
+      //gets purchase info from metadata
+      const { purchaseID, ownerID, email } = event.data.object.metadata
+      const paymentIntentID = event.data.object.id
+
+      //places all purchase info in object to be passed easily
+      const purchaseDetails = {
+        purchaseID: purchaseID,
+        ownerID: ownerID,
+        email: email, //if no email was passed in metadata, it was a mobile payment and was passed through the billing details
+        paymentIntentID: paymentIntentID,
+
+      }
+
+      //returns boolean value based on the success of recording purchaseDetails in firestore
+      const purchaseSuccesfullyHandled = await handlePurchaseSuccess(purchaseDetails);
+
+      if (purchaseSuccesfullyHandled) {
+        console.log('purchase succesfully handled, email is: ' + purchaseDetails.email);
+        res.send('success!')
+        res.status(200);
+        return;
+      }
+      //returns 400 status to retry in case there was a server error
+      else {
+        res.status(400);
+        res.send('The purchase information was not succesfully stored.');
+        return;
+      }
+    }
+    //returns 400 status to retry in case there was a server error
+    catch (err) {
+      console.error(err);
+      res.status(400);
+      res.send('failed');
+      return;
+    }
+  }
+
+  //this function only deals with successes, non successes will be sent away
+  else {
+    console.error(`Unhandled event type ${event.type}`);
+    res.send('error');
+    return;
+  }
+});
+
+//accepts purchase data, purchaseID and email and generates a receipt document
+//WILL RETURN FALSE IF ERROR
+const generateReceiptDocument = async (purchase, purchaseID, email, paymentIntentID = null) => {
+  const receiptRef = admin.firestore().collection(`receipts`).doc()
+
+  try {
+    console.log({
+      title: purchase.title,
+      caption: purchase.caption,
+      id: receiptRef.id,
+      ownerID: purchase.ownerID,
+      purchaseID: purchaseID,
+      recipientEmail: email,
+      paymentIntentID: paymentIntentID,
+    });
+
+    receiptRef.set({
+      title: purchase.title,
+      caption: purchase.caption,
+      id: receiptRef.id,
+      paymentIntentID: paymentIntentID,
+      ownerID: purchase.ownerID,
+      purchaseID: purchaseID,
+      recipientEmail: email,
+    });
+
+    return true;
+  }
+  catch (err) {
+    console.error(err);
+    return false;
+  }
+}
+
+//void async function taking in email
+//appends array to include email of sign up
+async function recordPurchaseEmail(purchaseDetails) {
+  const privatePurchaseDocRef = admin.firestore().doc(`sellers/${purchaseDetails.ownerID}/privateInfo/${purchaseDetails.purchaseID}`);
+
+  try {
+    privatePurchaseDocRef.get()
+      .then((docSnapshot) => {
+        //creates temp buyer array, pushes to it
+        //and then resets field to the new array
+        if (docSnapshot.exists) {
+
+          var buyerArray = docSnapshot.data().currentBuyerEmails;
+          console.log(buyerArray);
+          buyerArray.push(purchaseDetails.email);
+
+          privatePurchaseDocRef.update({
+            currentBuyerEmails: buyerArray
+          });
+
+        } else {
+          // create the document, add their email
+          // this will trigger if they are the first person to sign up
+          privatePurchaseDocRef.set({
+            currentBuyerEmails: [purchaseDetails.email]
+          })
+        }
+      });
+    return {
+      success: 'success',
+    }
+  }
+  catch (err) {
+    console.error(err);
+    return {
+      error: "There was an issue recording your email in our database."
+    }
+  };
+}
+
 //gets price of a purchase using purchaseID and uid depending on price
 const getPrice = async (purchaseID, uid, type, selectedPrice, promoCode) => {
   const purchaseDocRef = admin.firestore().doc(`sellers/${uid}/purchases/${purchaseID}`);
@@ -41,6 +185,35 @@ const getPrice = async (purchaseID, uid, type, selectedPrice, promoCode) => {
   }
 }
 
+//THIS FUNCTION WILL BE CALLED WHEN CHECKS TO ENSURE purchase CAN BE MADE PASS
+const handlePurchaseSuccess = async (purchaseDetails) => {
+  const uid = purchaseDetails.ownerID;
+  const purchaseID = purchaseDetails.purchaseID;
+  const email = purchaseDetails.email;
+  const paymentIntentID = purchaseDetails.paymentIntentID || null;
+  const purchaseDocRef = admin.firestore().doc(`sellers/${uid}/purchases/${purchaseID}`);
+
+  try {
+
+    //checks if private purchase document exists, creates it if necessary and records the email
+    const emailRecord = await recordPurchaseEmail(purchaseDetails);
+    if (emailRecord.error) {
+      return false;
+    }
+
+    const purchaseDocData = await purchaseDocRef.get();
+    receiptGeneratedSuccesfully = await generateReceiptDocument(purchaseDocData.data(), purchaseID, email, paymentIntentID);
+
+    //returns true/false depending on the success
+    return receiptGeneratedSuccesfully;
+  }
+  catch (err) {
+    console.log("Error editing documents in firestore inside handlePurchaseSuccess");
+    console.error(err);
+    return false
+  }
+
+}
 /*
 HTTPS FUNCTION TO ALLOW FOR FREE SIGN UP GOES HERE
 */
@@ -277,6 +450,33 @@ exports.getStripeAccountLink = functions.https.onCall(async (data, context) => {
   })
 });
 
+//cc sellers by uid
+exports.onReceiptCreation = functions.firestore.document('receipts/{receiptId}')
+  .onCreate(async (snapshot, context) => {
+    const itemDataSnap = await snapshot.ref.get()
+    const { title, recipientEmail, ownerID, purchaseID, caption, id } = itemDataSnap.data()
+    let eventObject;
+
+    //sends an email to recipient, cc the seller
+    try {
+      console.log('Generating a receipt...');
+
+      const sellerRef = await admin.firestore().doc(`sellers/${ownerID}`).get()
+      const sellerData = sellerRef.data();
+
+      if (caption) {
+        eventObject.caption = "Caption:\n" + caption;
+      }
+
+
+      
+
+    }
+    catch (err) {
+      console.error(err)
+    }
+  });
+
 exports.submitEditProfile = functions.https.onCall(async (data, context) => {
   const db = admin.firestore();
   const uid = context.auth.uid;
@@ -330,5 +530,27 @@ exports.submitEditProfile = functions.https.onCall(async (data, context) => {
 
   return {
     success: true
+  }
+});
+
+exports.verifyPromoCode = functions.https.onCall(async (data, context) => {
+  const promoCodesRef = admin.firestore().doc(`sellers/${data.uid}/purchases/${data.purchaseID}`).collection("promoCodes");
+
+  try {
+    const promoCodesGet = await promoCodesRef.get();
+    const promoCodes = promoCodesGet.docs.map(doc => doc.data());
+    let result;
+
+    promoCodes.forEach(code => {
+      if (code.code === data.promoCode) {
+        result = code;
+      }
+    });
+    return result;
+  }
+  catch (err) {
+    console.log("Error in verifying promo code");
+    console.error(err);
+    return false
   }
 });
